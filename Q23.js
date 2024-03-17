fecc9e141401e94ce0f2c225176b2ab4c7958c8d
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 04:02:30 -0700

    Add files via upload
    Q 23 Node.js of 45 Assignments

---
+++ Q23.js
@@ -0,0 +1,29 @@
+"use strict";
+// 23. Conditional Tests: Write a series of conditional tests. Print a statement
+// describing each test and your prediction for the results of each test. Your code
+// should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True otherwise I pridict fales")
+console.log(car == 'subaru');
+console.log(car == 'civic');
+let num = 25;
+// console.log("Is num == 25 (in number) ? I ptidict true ontherwise I ptidict fales")
+console.log(num === 25);
+console.log(num === "25");
+let person = "Sadiq";
+console.log("Is person == Sadiq? I ptidict true ontherwise I ptidict fales");
+console.log(person == "Sadiq");
+console.log(person == "Saad");
+let fruit = "Apple";
+// console.log("Is Fruit == Apple ? I ptidict true ontherwise I pridict fales")
+console.log(fruit == "Apple");
+console.log(fruit == "Banana");
+let age = 23;
+// ("Is age == 23 (in number) ? I ptidict true ontherwise I ptidict fales")
+console.log(age === 23);
+console.log(age === 24);
